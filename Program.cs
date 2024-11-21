f106f2100b480b0a26e19e072dc934dd452f58ef
phillip deleon <phillipdeleon68@gmail.com>
2024-11-21 14:01:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ChallengeLabs_Week3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to see if it is a palindrome: ");
+            string input = Console.ReadLine();
+
+            
+
+            if (Palindrome(input))
+            {
+                Console.WriteLine($"'{input}' is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"'{input}' is not a palindrome.");
+            }
+        
+
+        }
+
+        static public bool Palindrome(string input)
+        {
+
+           int length = input.Length;
+
+            for(int i = 0; i < input.Length / 2; i++)  
+            {
+               if (input[i] != input[length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
